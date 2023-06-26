ce89232eeb059ba4ce9309f3f2b6e21b4a053f11
AndrewYao <andrewyao@anasystem.com.tw>
2023-06-26 11:51:49 +0800

    test: test swiper

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,52 @@
 <template>
-	<div class="about">
-		<div class="owen text-4xl text-yellow-200">This is an about page</div>
+	<div class="w-[800px] h-[500px] bg-red-200">
+		<swiper-container
+			class="h-full"
+			:slides-per-view="3"
+			:space-between="spaceBetween"
+			:centered-slides="true"
+			:pagination="{
+				hideOnClick: true,
+			}"
+			:breakpoints="{
+				768: {
+					slidesPerView: 3,
+				},
+			}"
+			@progress="onProgress"
+			@slidechange="onSlideChange"
+		>
+			<swiper-slide class="bg-green-100">Slide 1</swiper-slide>
+			<swiper-slide class="bg-green-200">Slide 2</swiper-slide>
+			<swiper-slide class="bg-green-300">Slide 3</swiper-slide>
+		</swiper-container>
+
+		<div ref="target">123456789</div>
 	</div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-	.about {
-		min-height: 100vh;
-		display: flex;
-		align-items: center;
-	}
-}
-</style>
+<script setup>
+import { register } from 'swiper/element/bundle';
+import { useMouseInElement, useEventListener } from '@vueuse/core';
+import { ref, onMounted } from 'vue';
+register();
 
-<style lang="scss" scoped>
-$color: #fcba03;
-.about {
-	background-color: $color;
-	margin: 50px;
+const spaceBetween = 10;
+const onProgress = (e) => {
+	const [swiper, progress] = e.detail;
+	console.log(progress);
+};
 
-  .owen {
-    background-color: green;
-  }
-}
-</style>
+const onSlideChange = (e) => {
+	console.log('slide changed');
+};
+
+const target = ref(null);
+const { isOutside } = useMouseInElement(target);
+
+onMounted(() => {
+	useEventListener(document, 'click', () => {
+		console.log('clicked');
+	});
+});
+</script>
